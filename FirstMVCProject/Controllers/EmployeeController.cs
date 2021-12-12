24162757710db821cf4a3c5dc1867951644dff21
Yunus İdris Yeşil <yunusyesil85@gmail.com>
2021-12-12 18:02:45 +0300

    Added missing fields to the Create view. Update/Delete implemented for Employee

--- FirstMVCProject/Controllers/EmployeeController.cs
+++ FirstMVCProject/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using FirstMVCProject.Models;
 using FirstMVCProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace FirstMVCProject.Controllers
@@ -40,6 +41,14 @@ namespace FirstMVCProject.Controllers
             {
                 FirstName = model.FirstName,
                 LastName = model.LastName,
+                Title = model.Title,
+                BirthDate = model.BirthDate,
+                HireDate = DateTime.Now,
+                Address = model.Address,
+                City = model.City,
+                Country = model.Country,
+                HomePhone = model.HomePhone,
+                TitleOfCourtesy = model.TitleOfCourtesy,
             };
 
             try
@@ -53,6 +62,80 @@ namespace FirstMVCProject.Controllers
                 ModelState.AddModelError(string.Empty, $"An error accured while adding {employee.FirstName}");
                 return View(model);
             }
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.EmployeeId == id);
+
+            try
+            {
+                _context.Employees.Remove(employee);
+                _context.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return RedirectToAction(nameof(Detail),new { id = employee.EmployeeId});
+                throw;
+            }
+            TempData["Deleted_Employee"] = employee.FirstName + employee.LastName;
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Update(int? id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.EmployeeId == id);
+            if (employee == null) return RedirectToAction(nameof(Index));
+
+            var model = new EmployeeViewModel()
+            {
+                EmployeeId = employee.EmployeeId,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                BirthDate = (DateTime)employee.BirthDate,
+                Title = employee.Title,
+                TitleOfCourtesy = employee.TitleOfCourtesy,
+                Address = employee.Address,
+                City = employee.City,
+                Country = employee.Country,
+                HireDate = (DateTime)employee.HireDate,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(EmployeeViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var employee = _context.Employees.FirstOrDefault(x => x.EmployeeId == model.EmployeeId);
+
+            try
+            {
+                employee.FirstName = model.FirstName;
+                employee.LastName = model.LastName;
+                employee.BirthDate = model.BirthDate;
+                employee.Title = model.Title;
+                employee.Address = model.Address;
+                employee.City = model.City;
+                employee.Country = model.Country; 
+                employee.HomePhone = model.HomePhone;
+                employee.TitleOfCourtesy= model.TitleOfCourtesy;
+
+                _context.Employees.Update(employee);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Detail), new { id = employee.EmployeeId });
+            }
+            catch (System.Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return View(model);
         }
     }
 }
--- FirstMVCProject/ViewModels/EmployeeViewModel.cs
+++ FirstMVCProject/ViewModels/EmployeeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FirstMVCProject.ViewModels
 {
@@ -12,8 +13,33 @@ namespace FirstMVCProject.ViewModels
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Employee lastname cannot be empty")]
-        [StringLength(20,ErrorMessage = "Employee lastname cannot exceed 20 charaters")]
+        [StringLength(20,ErrorMessage = "Employee lastname cannot exceed 20 characters")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Employee birthdate cannot be empty")]
+        [Display(Name = "Birthdate")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage ="Employee title cannot be empty")]
+        [StringLength(30,ErrorMessage = "Employee title cannot exceed 30 characters")]
+        public string Title { get; set; }
+
+        [StringLength(60,ErrorMessage = "Employee address cannot exceed 60 characters")]
+        public string Address { get; set; }
+
+        [StringLength(15,ErrorMessage = "City name cannot exceed 15 characters")]
+        public string City { get; set; }
+
+        [StringLength(15,ErrorMessage = "Country name cannot exceed 15 characters")]
+        public string Country { get; set; }
+        
+        [StringLength(24,ErrorMessage = "Home Phone cannot exceed 24 characters")]
+        public string HomePhone { get; set; }
+
+        [StringLength(25,ErrorMessage = "Title Of Courtesy cannot exceed 25 characters")]
+        public string TitleOfCourtesy { get; set; }
+
+        public DateTime HireDate { get; set; }
     }
 }
