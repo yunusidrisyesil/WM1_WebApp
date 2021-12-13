e60decb6add9246374534aea5e8517fc5b22b8f5
AniueGreen <yunusyesil85@gmail.com>
2021-12-13 19:16:13 +0300

    Product Update implemented

--- FirstMVCProject/Controllers/ProductController.cs
+++ FirstMVCProject/Controllers/ProductController.cs
@@ -1,6 +1,10 @@
 ﻿using FirstMVCProject.Models;
+using FirstMVCProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FirstMVCProject.Controllers
@@ -9,20 +13,136 @@ namespace FirstMVCProject.Controllers
     {
 
         private readonly NorthwindContext _context;
+        private int _pageSize = 10;
 
         public ProductController(NorthwindContext context)
         {
             _context = context;
         }
 
-        public IActionResult Index(int? id)
+        public IActionResult Detail(int? id)
         {
-            var product = _context.Products.Include(x => x.Supplier).FirstOrDefault(x => x.ProductId == id);
-            if (product == null)
+            //var product = _context.Products.Include(x => x.Supplier).FirstOrDefault(x => x.ProductId == id);
+            //if (product == null)
+            //{
+            //    return RedirectToAction(nameof(Index), "Home");
+            //}
+            //return View(product);
+
+            var data = _context.Products.Include(x => x.Supplier).Include(x => x.Category).FirstOrDefault(x => x.ProductId == id);
+            if (data == null) return RedirectToAction(nameof(Index));
+
+            var model = new ProductViewModel
             {
-                return RedirectToAction(nameof(Index), "Home");
+                ProductId = data.ProductId,
+                CategoryName = data.Category?.CategoryName,
+                CompanyName = data.Supplier?.CompanyName,
+                ProductName = data.ProductName,
+                UnitPrice = data.UnitPrice,
+                SupplierId = data.SupplierId,
+                CategoryId = data.CategoryId,
+            };
+            return View(model);
+
+        }
+
+        public IActionResult Index(int? page = 1)
+        {
+            var model = _context.Products.Include(x => x.Category)
+                                         .Include(x => x.Supplier)
+                                         .OrderBy(x => x.Category.CategoryName)
+                                         .ThenBy(x => x.ProductName)
+                                         .Skip((page.GetValueOrDefault() - 1) * _pageSize)
+                                         .Take(_pageSize)
+                                         .ToList();
+
+            ViewBag.Page = page.GetValueOrDefault(1);
+            ViewBag.Limit =  (int)Math.Ceiling(_context.Products.Count() / (double)_pageSize);
+            //ViewBag.Categories = _context.Categories.OrderBy(x => x.CategoryName).ToList();
+            //ViewBag.Product = _context.Suppliers.OrderBy(x => x.CompanyName).ToList();
+
+            return View(model);
+        }
+
+        public IActionResult Update(int? id)
+        {
+            var data = _context.Products.Include(x => x.Supplier).Include(x => x.Category).FirstOrDefault(x => x.ProductId == id);
+            if (data == null) return RedirectToAction(nameof(Index));
+
+            var model = new ProductViewModel
+            {
+                ProductId = data.ProductId,
+                CategoryName = data.Category?.CategoryName,
+                CompanyName = data.Supplier?.CompanyName,
+                ProductName = data.ProductName,
+                UnitPrice = data.UnitPrice,
+                SupplierId = data.SupplierId,
+                CategoryId = data.CategoryId,
+            };
+
+            var categories = _context.Categories.OrderBy(x => x.CategoryName).ToList();
+            var suppliers = _context.Suppliers.OrderBy(x => x.CompanyName).ToList();
+
+            var categoryList = new List<SelectListItem>() 
+            {
+                new SelectListItem("No Category", null),
+            };
+
+            var supplierList = new List<SelectListItem>()
+            {
+                new SelectListItem("No Supllier",null)
+            };
+
+            foreach (var category in categories)
+            {
+                categoryList.Add(new SelectListItem(category.CategoryName, category.CategoryId.ToString()));
             }
-            return View(product);
+
+            foreach (var supplier in suppliers)
+            {
+                supplierList.Add(new SelectListItem(supplier.CompanyName, supplier.SupplierId.ToString()));
+            }
+
+            ViewBag.CategoryList = categoryList;
+            ViewBag.SupplierList = supplierList;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(ProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var product = _context.Products.FirstOrDefault(x => x.ProductId == model.ProductId);
+            if (product == null)
+                return RedirectToAction(nameof(Index));
+
+            try
+            {
+                product.ProductId = model.ProductId;
+                product.ProductName = model.ProductName;
+                product.UnitPrice = model.UnitPrice;
+                product.SupplierId = model.SupplierId;
+                product.CategoryId = model.CategoryId;
+
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                TempData["Mesaj"] = $"{product.ProductName} başarıyla güncellenmiştir";
+                return RedirectToAction(nameof(Detail), new { id = product.ProductId });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(model);
+            }
+        }
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 }
---
+++ FirstMVCProject/ViewModels/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCProject.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int? SupplierId { get; set; }
+        public int? CategoryId { get; set; }
+        public string CategoryName{ get; set; }
+        public string CompanyName { get; set; }
+        [Range(0,99999999999,ErrorMessage = "Product unit price has to be between 0-99999999999")]
+        public decimal? UnitPrice { get; set; }
+
+    }
+}
