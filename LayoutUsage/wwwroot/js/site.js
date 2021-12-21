bc32f3d7172661247f1ece87c818bc739f41fb15
Yunus İdris Yeşil <yunusyesil85@gmail.com>
2021-12-21 20:45:05 +0300

    Layout/JS/Css added

---
+++ LayoutUsage/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿console.log("MVC apllication started");
+
+var App = {
+    name : "MVC application",
+    welcome: function () {
+        return "Hello " + this.name;
+    }
+}
+
+//console.log(App.welcome);
