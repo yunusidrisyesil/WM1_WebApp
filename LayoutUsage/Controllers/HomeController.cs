803f3cf180f8b7f761b51a659fd01569fe212130
Yunus İdris Yeşil <yunusyesil85@gmail.com>
2021-12-21 18:22:44 +0300

    LayoutUsage Project added

---
+++ LayoutUsage/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using LayoutUsage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LayoutUsage.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            var model = new List<Product>()
+            {
+                new Product()
+                {
+                    Id = 1,
+                    Name = "Watermelon",
+                    Price = 7
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Name = "Plum",
+                    Price = 9
+                }
+            };
+            return View(model);
+        }
+    }
+}
