c760a4e5284ab0a8117d00c3b2920dde50648427
Yunus İdris Yeşil <yunusyesil85@gmail.com>
2021-12-22 20:57:45 +0300

    service configs set. ViewModel created

--- ItServiceApp/Startup.cs
+++ ItServiceApp/Startup.cs
@@ -1,6 +1,10 @@
+using ItServiceApp.Data;
+using ItServiceApp.Models.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +16,41 @@ namespace ItServiceApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MyContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlConnection"));
+            });
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireDigit = true;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 3;
+                options.Lockout.AllowedForNewUsers = false;
+                
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options => {
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ ItServiceApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItServiceApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="Name is required")]
+        [Display(Name="Name")]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Surname is required")]
+        [Display(Name = "Surname")]
+        [StringLength(50)]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(100,MinimumLength = 6,ErrorMessage ="Password must be at least 6 chracter")]
+        public string Password{ get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 chracter")]
+        [Compare(nameof(Password),ErrorMessage ="Passwords are not same")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
